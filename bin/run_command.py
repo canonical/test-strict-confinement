083d1d5becc099bc6062160e6784e2d6eb2020c8
rickwu4444 <rick.wu@canonical.com>
2024-06-26 13:26:20 +0800

    Re-format to black

--- bin/run_command.py
+++ bin/run_command.py
@@ -20,29 +20,30 @@ logging.basicConfig(
 
 
 class RunnableFunction:
-
     @staticmethod
     def warm_boot(args):
-        cmd = 'dbus-send --system --dest=org.freedesktop.login1 --print-reply \
+        cmd = "dbus-send --system --dest=org.freedesktop.login1 --print-reply \
         /org/freedesktop/login1 org.freedesktop.login1.Manager.Reboot \
-        boolean:true'
+        boolean:true"
         run(cmd, args.timeout)
 
     @staticmethod
     def cold_boot(args):
-        cmd = 'dbus-send --system --dest=org.freedesktop.login1 --print-reply \
+        cmd = "dbus-send --system --dest=org.freedesktop.login1 --print-reply \
         /org/freedesktop/login1 org.freedesktop.login1.Manager.PowerOff \
-        boolean:true'
+        boolean:true"
         run(cmd, args.timeout)
 
 
 def run(cmd, timeout):
     logging.info(cmd)
-    process = sp.run(shlex.split(cmd),
-                     stdout=sp.PIPE,
-                     stderr=sp.PIPE,
-                     text=True,
-                     timeout=timeout)
+    process = sp.run(
+        shlex.split(cmd),
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+        text=True,
+        timeout=timeout,
+    )
     if process.returncode != 0:
         logging.error("Return message: {}".format(process.stderr.strip()))
         raise SystemExit("Command run failed")
@@ -55,16 +56,25 @@ def main():
     for key, func in getmembers(RunnableFunction, isfunction):
         func_mapping[key] = func
     parser = argparse.ArgumentParser(
-        description='This script is for calling subprocess for testing.')
-    parser.add_argument('function',
-                        help='The name of test function.',
-                        choices=func_mapping)
-    parser.add_argument("-a", "--argument", required=False,
-                        help="The argument for the function")
-    parser.add_argument("-t", "--timeout", required=False,
-                        type=int,
-                        default=60,
-                        help="Subprocess timeout period.")
+        description="This script is for calling subprocess for testing."
+    )
+    parser.add_argument(
+        "function", help="The name of test function.", choices=func_mapping
+    )
+    parser.add_argument(
+        "-a",
+        "--argument",
+        required=False,
+        help="The argument for the function",
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        required=False,
+        type=int,
+        default=60,
+        help="Subprocess timeout period.",
+    )
     args = parser.parse_args()
 
     args.timeout = 60 if not args.timeout else args.timeout
